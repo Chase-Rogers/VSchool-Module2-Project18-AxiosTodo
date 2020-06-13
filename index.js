234d0614da2a8a54829aa37107cb912a14fcd794
Chase <sde.chase.rogers@gmail.com>
2020-06-13 14:43:53 -0600

    good enough to pass. Will revisit later to improve

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 const getData = () => 
     axios
         .get("https://api.vschool.io/chaserogers/todo")
-        // .then((response) => console.log(response.data))
         .then((response) => listData(response.data))
         .catch((error) => console.log(error));
 
-
 getData();
-
-axios
-    .get("https://api.vschool.io/chaserogers/todo")
-    .then((response) => {
-        // for(let i = 0; i < response.data.length; i++){
-        //     const h1 = document.createElement('h1')
-        //     h1.textContent = response.data[i].title
-        //     document.body.appendChild(h1)
-        // }
-    })
-    .catch((error) => console.log(error));
 
 const todoForm = document.todoform
 const updateform = document.updateform
+
+const createForm = () => {
+    const newform = document.createElement('form')
+    const h1 = document.createElement('h1')
+    const img = document.createElement('img')
+    const p = document.createElement('p')
+    const price = document.createElement('p')
+    const status = document.createElement('input')
+    const button = document.createElement('button')
+    status.setAttribute('type', 'checkbox')
+    status.setAttribute('onchange', 'completeToDo(event)')
+    button.setAttribute('onclick', 'deleteToDo(event)')
+    button.textContent = "Delete"
+    newform.appendChild(h1)
+    newform.appendChild(status)
+    newform.appendChild(p)
+    newform.appendChild(img)
+    newform.appendChild(price)
+    newform.appendChild(button)
+
+    return newform;
+}
+
 // Add Record
 const newTodo = (event) => {
     event.preventDefault();
@@ -29,10 +39,26 @@ const newTodo = (event) => {
         title: todoForm.title.value,
         description: todoForm.description.value,
         imgUrl: todoForm.imgUrl.value,
+        price: todoForm.price.value
     };
+
+    const newform = createForm();
+    newform.getElementsByTagName('h1')[0].innerHTML = todoForm.title.value
+    newform.getElementsByTagName('p')[0].innerHTML = `Description: ${todoForm.description.value}`
+    newform.getElementsByTagName('img')[0].src = todoForm.imgUrl.value
+    newform.getElementsByTagName('p')[1].textContent = `Price: ${todoForm.price.value}`
+    todoForm.title.value = ''
+    todoForm.description.value = ''
+    todoForm.imgUrl.value = ''
+    todoForm.price.value = '';
     axios
         .post("https://api.vschool.io/chaserogers/todo", newToDo)
-        .then((response) => getData())
+        .then((response) => {
+            newform.getElementsByTagName('input')[0].textContent = response.data._id;
+            document.getElementById(`todo-list`).appendChild(newform)
+
+
+        })
         .catch((error) => console.log(error));
 };
 
@@ -40,8 +66,6 @@ const newTodo = (event) => {
 const deleteToDo = (event) => {
     event.preventDefault();
     const id = event.target.parentElement.children[1].textContent
-    console.log(id)
-    
 
     axios.delete(`https://api.vschool.io/chaserogers/todo/${id}`)
         .then(response => event.target.parentElement.remove())
@@ -49,62 +73,22 @@ const deleteToDo = (event) => {
         .catch(error => console.log(error))
 };
 
-// const updateToDo = (event) => {
-//     event.preventDefault();
-//     const id = document.updateform.id.value
-//     const updates = {
-//         title: updateform.title.value,
-//         completed: true,
-//         description: updateform.description.value,
-//         price: updateform.price.value,
-//         imgUrl: updateform.imgUrl.value,
-//     }
-//     axios.put(`https://api.vschool.io/chaserogers/todo/${id}`, updates)
-//         .then(response => getData())
-//         .catch(error => error)
-// }
-
 const listData = data => {
     clearList();
     for(let i = 0; i < data.length; i++){
-        const h1 = document.createElement('h1')
-        const p = document.createElement('p')
-        const img = document.createElement('img')
-        const price = document.createElement('p')
-        const status = document.createElement('input')
-        const newform = document.createElement('form')
-        const button = document.createElement('button')
-        button.setAttribute('onclick', 'deleteToDo(event)')
-        button.textContent = "Delete"
-        newform.setAttribute('name', `newform${i}`)
-        newform.setAttribute('id', `newform${i}`)
-        status.setAttribute('type', 'checkbox')
-        status.setAttribute('onchange', 'completeToDo(event)')
-
+        const newform = createForm();
+        newform.getElementsByTagName('h1')[0].textContent = `Title: ${data[i].title}`
+        newform.getElementsByTagName('p')[0].textContent = `Description: ${data[i].description}`
+        newform.getElementsByTagName('img')[0].src = data[i].imgUrl
+        newform.getElementsByTagName('p')[1].textContent = `Price: ${data[i].price}`
         if (data[i].completed) {
-            h1.innerHTML = `Title: ${data[i].title.strike()}`
-            p.innerHTML = `Description: ${data[i].description.strike()}`
-            img.textContent = data[i].imgUrl.strike()
-            price.textContent = `Price: ${data[i].price}`
+            newform.classList.add('complete');
+            newform.getElementsByTagName('input')[0].setAttribute("checked", true);
         } else {
-            h1.textContent = `Title: ${data[i].title}`
-            p.textContent = `Description: ${data[i].description}`
-            img.src = data[i].imgUrl
-            price.textContent = `Price: ${data[i].price}`
+            newform.getElementsByTagName('input')[0].removeAttribute("checked")
         }
-        status.textContent = data[i]._id
-        if (data[i].completed === true) {
-            status.setAttribute("checked", true);
-        } else {
-            status.removeAttribute("checked")
-        }
+        newform.getElementsByTagName('input')[0].textContent = data[i]._id
         document.getElementById(`todo-list`).appendChild(newform)
-        document.getElementById(`newform${i}`).appendChild(h1)
-        document.getElementById(`newform${i}`).appendChild(status)
-        document.getElementById(`newform${i}`).appendChild(p)
-        document.getElementById(`newform${i}`).appendChild(img)
-        document.getElementById(`newform${i}`).appendChild(price)
-        document.getElementById(`newform${i}`).appendChild(button)
     }
 }
 
@@ -117,10 +101,10 @@ const clearList = () => {
 
 const completeToDo = event => {
     const form = event.currentTarget.form.id;
-    const id = event.currentTarget.textContent
-    console.log(event.target.checked)
-    console.log('test');
+    const id = event.currentTarget.textContent;
+    (event.target.checked ? event.target.parentElement.classList.add('complete') : event.target.parentElement.classList.remove('complete'))
+    
     axios.put(`https://api.vschool.io/chaserogers/todo/${id}`, {completed: event.target.checked})
-        .then(response => getData())
+        .then(response => {})
         .catch(error => error)
 }
